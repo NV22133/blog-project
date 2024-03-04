ebba07391ad69a2c71f5c5335da6fae37223c237
NV22133 <155047853+NV22133@users.noreply.github.com>
2024-03-04 09:28:36 +0000

    CTA

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,17 +3,13 @@ import HeroImage from "../assets/HeroImage.svg";
 import Post1Image from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.svg";
 import CtaImage from "../assets/CtaImage.jpg";
-const samplePostImage = "/images/sample.jpg";
-const userImage = "/images/user.png";
 
 const images = {
   Logo,
   HeroImage,
   Post1Image,
   PostProfileImage,
-  CtaImage,
-  samplePostImage,
-  userImage,
+  CtaImage
 };
 
 export default images;
