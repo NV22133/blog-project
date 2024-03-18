48e896b17686d9afa09f1b2375a27035a58ad471
NV22133 <155047853+NV22133@users.noreply.github.com>
2024-03-18 08:45:07 +0000

    register

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
-import ArticleDetailPage from "./pages/articelDetail/ArticleDetailPage";
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/HomePage";
+import RegisterPage from "./pages/register/RegisterPage";
 
 function App() {
   return (
@@ -9,9 +11,10 @@ function App() {
       <Routes>
         <Route index path="/" element={<HomePage />} />
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   );
-};
+}
 
 export default App;
