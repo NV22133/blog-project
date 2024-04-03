0953e6f189e94ff2e5f33e22eb0ee8db2d098085
NV22133 <155047853+NV22133@users.noreply.github.com>
2024-04-03 09:11:32 +0000

    show all posts in home page

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,7 +2,9 @@ import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg";
 import Post1Image from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.svg";
-import CtaImage from '../assets/CtaImage.png'
+import CtaImage from "../assets/CtaImage.png";
+const samplePostImage = "/images/sample.jpg";
+const userImage = "/images/user.png";
 
 const images = {
   Logo,
@@ -10,6 +12,8 @@ const images = {
   Post1Image,
   PostProfileImage,
   CtaImage,
+  samplePostImage,
+  userImage,
 };
 
 export default images;
